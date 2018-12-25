d80afdcfed9a4c69fbf69cd8b44fa072db716f50
zxhuaman <zxhuaman@gmail.com>
2018-12-25 22:29:03 +0800

    add:layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,24 +9,8 @@ export class AppComponent {
   // @ts-ignore
   private md = window.markdownit();
   renderHtml = '';
-  private selection = window.getSelection();
-  private range: Range;
-  left = 0;
-  top = 0;
-
-  onClick($event: any) {
-    console.log(event);
-    console.log(window.getSelection());
-    this.left = $event.clientX - 7;
-    this.top = $event.clientY - 28;
-  }
-
-  onChange($event: Event): void {
-    console.log('onChange', event);
-  }
 
   onInput(ev: any): void {
-    console.log(ev.data.length);
     this.renderHtml = this.md.render(ev.target.value);
   }
 }
