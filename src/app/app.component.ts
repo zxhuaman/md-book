5675a87c4ee2e607b680e9a338646af0468711e5
zxhuaman <zxhuaman@gmail.com>
2019-01-04 19:01:27 +0800

    modify:markdown service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Tool} from './tool';
 import {insertText} from './utils';
 import {EDIT_TOOLS} from './edit.operation';
+import {MarkdownService} from './markdown.service';
 
 
 @Component({
@@ -10,12 +11,13 @@ import {EDIT_TOOLS} from './edit.operation';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-
-  // @ts-ignore
-  private md = window.markdownit();
   renderHtml = '';
   textarea: HTMLTextAreaElement;
   tools: Array<Tool> = EDIT_TOOLS;
+
+  constructor(private service: MarkdownService) {
+  }
+
 
   ngOnInit(): void {
     // @ts-ignore
@@ -23,10 +25,11 @@ export class AppComponent implements OnInit {
   }
 
   onChange(text: string) {
-    this.renderHtml = this.md.render(text);
+    this.renderHtml = this.service.render(text);
   }
 
   onClickTool(tool: Tool): void {
     insertText(this.textarea, tool.prefix, tool.text, tool.suffix);
+    this.onChange(this.textarea.value);
   }
 }
--- src/app/markdown.service.ts
+++ src/app/markdown.service.ts
@@ -4,15 +4,34 @@ import {Injectable} from '@angular/core';
   providedIn: 'root'
 })
 export class MarkdownService {
-
-  // @ts-ignore
-  private md = window.markdownit();
+  private md;
 
   constructor() {
     // @ts-ignore
-    this.md
-      .use(window.markdownitSup())
-      .use();
+    this.md = window.markdownit();
+    // @ts-ignore
+    window.markdownitAbbr(this.md);
+    // @ts-ignore
+    window.markdownitContainer(this.md);
+    // @ts-ignore
+    window.markdownitDeflist(this.md);
+    // @ts-ignore
+    window.markdownitEmoji(this.md);
+    // @ts-ignore
+    window.markdownitFootnote(this.md);
+    // @ts-ignore
+    window.markdownitIns(this.md);
+    // @ts-ignore
+    window.markdownitMark(this.md);
+    // @ts-ignore
+    window.markdownitSub(this.md);
+    // @ts-ignore
+    window.markdownitSup(this.md);
+    // @ts-ignore
+    window.markdownitTaskLists(this.md);
   }
 
+  render(text: string): string {
+    return this.md.render(text);
+  }
 }
