5facf2df564b3dbbcbbf1394094aede94c9f35b8
zxhuaman <zxhuaman@gmail.com>
2019-02-14 16:47:16 +0800

    add: login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {EditComponent} from './edit/edit.component';
+import {LoginComponent} from './login/login.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'edit', pathMatch: 'full'},
-  {path: 'edit', component: EditComponent}
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: 'edit', component: EditComponent},
+  {path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {NgZorroAntdModule, NZ_I18N, NzButtonModule, NzLayoutModule, zh_CN} from 'ng-zorro-antd';
-import { FormsModule } from '@angular/forms';
+import {
+  NgZorroAntdModule,
+  NZ_I18N,
+  NzButtonModule,
+  NzCheckboxModule,
+  NzFormModule,
+  NzInputModule,
+  NzLayoutModule,
+  zh_CN
+} from 'ng-zorro-antd';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { registerLocaleData } from '@angular/common';
 import zh from '@angular/common/locales/zh';
 import { EditComponent } from './edit/edit.component';
+import { LoginComponent } from './login/login.component';
 
 registerLocaleData(zh);
 
 @NgModule({
   declarations: [
     AppComponent,
-    EditComponent
+    EditComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -27,7 +38,11 @@ registerLocaleData(zh);
     BrowserAnimationsModule,
     NzLayoutModule,
     HttpClientModule,
-    NzButtonModule
+    NzButtonModule,
+    NzFormModule,
+    NzInputModule,
+    NzCheckboxModule,
+    ReactiveFormsModule
   ],
   providers: [{ provide: NZ_I18N, useValue: zh_CN }],
   bootstrap: [AppComponent]
